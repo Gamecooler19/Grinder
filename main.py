db910e525b3dcf12bc3d118f2de72aca05a91a89
Gamecooler19 <Gamecooler19@hotmail.com>
2022-03-10 21:51:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ client = commands.Bot(command_prefix=prefix,
                    self_bot=True)
 
 @client.command()
-async def help(ctx):
-  embed = discord.Embed(title="Gamecooler19's Auto OwO", color=420699, description=f"**{prefix}start**\n Sends A Trick Made By Gamecooler19 In Every 50 seconds.\n\n**{prefix}stop**\n Stops Auto OwO.")
-  embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/912767456081235999/a_50d94ee7e9b7a2ccefd649bafbcb1a33.gif?size=1024")
+async def help(ctx, colour, title, description, *,url):
+  embed = discord.Embed(title="Gamecooler19's Grinder Self Bot", type='rich', color=420699, description=f"**{prefix}owostart**\n Sends A OwO Trick Made By Gamecooler19 In Every 50 seconds.\n\n**{prefix}owostop**\n Stops Auto OwO. \n\n\n\n**{prefix}dmcstart**\n Sends A Dank Memer Trick Made By Gamecooler19 In Every 50 seconds.\n\n**{prefix}dmcstop**\n Stops Auto Dank Memer.")
+  embed.set_thumbnail(url="https://cdn.discordapp.com/icons/917333971061329930/a_dd370765088466ccd900412d75eb29d9.gif?size=1024")
   await ctx.send(embed=embed)
 
 @client.command(pass_context=True)
-async def start(ctx):
+async def owostart(ctx):
 	await ctx.message.delete()
-	await ctx.send('Auto OwO is now **enabled**!')
+	await ctx.send('Auto OwO Is Now **enabled**!')
 	global owocs
 	owocs = True
 	while owocs:
@@ -51,20 +51,44 @@ async def start(ctx):
 			 await asyncio.sleep(17)
 			 await ctx.send('owo cash')
 			 print(f"{Fore.GREEN}succefully balanced")
-       
-
-
 
 @client.command()
-async def stop(ctx):
+async def owostop(ctx):
 	await ctx.message.delete()
 	await ctx.send('Auto OwO Is Now **Disabled**!')
 	global owocs
 	owocs = False
 
+
+@client.command(pass_context=True)
+async def dmcstart(ctx):
+	await ctx.message.delete()
+	await ctx.send('Auto Dank Memer Is Now **Enabled**!')
+	global dmcs
+	dmcs = True
+	while dmcs:
+		async with ctx.typing():
+			await asyncio.sleep(3)
+			await ctx.send('pls beg')
+			print(f"{Fore.GREEN}succesfully begged")
+			await ctx.send('pls fish')
+			print(f"{Fore.GREEN}succesfully fished") 
+			await ctx.send('pls hunt')
+			print(f"{Fore.GREEN}succesfully hunt")
+			await ctx.send('pls dep all')
+			print(f"{Fore.GREEN}succesfully deposited all")
+			await asyncio.sleep(47)
+			
+@client.command()
+async def dmcstop(ctx):
+	await ctx.message.delete()
+	await ctx.send('Auto Dank Memer Is Now **Disabled**!')
+	global owocs
+	owocs = False
+
 @client.event
 async def on_ready():
-  activity = discord.Game(name="Minecraft", type=4)
+  activity = discord.ActivityType.watching(name="Gamecooler19 On Youtube")
   await client.change_presence(status=discord.Status.dnd, activity=activity)
   print(f'''{Fore.RED}
                                                                         ▄▄                                
